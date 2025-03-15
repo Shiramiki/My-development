0edf4ab31dc57cd5c4a476a9167cb3dfafd14b49
Isooba Mbeiza Rachel <123367383+Shiramiki@users.noreply.github.com>
2025-03-15 12:08:59 +0300

    Updates

--- React/Day 1/first_child/src/App.js
+++ React/Day 1/first_child/src/App.js
@@ -1,14 +1,26 @@
 
 import './App.css';
+import React, { Component } from 'react'
 import Greet from './components/greet'
-
-function App() {
-  return (
-    <div className="App">
-      <Greet></Greet>
-      
-    </div>
-  );
+import Welcome from './components/Welcome'
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Greet></Greet>
+        <Welcome></Welcome>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Greet></Greet>
+//     </div>
+//   );
+// }
+
 
 export default App;
