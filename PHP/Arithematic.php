6e426413c393b31caf2cdfc331b6a6be0001d1b6
Isooba Mbeiza Rachel <123367383+Shiramiki@users.noreply.github.com>
2025-06-03 18:04:28 +0300

    lesson 3

---
+++ PHP/Arithematic.php
@@ -0,0 +1,17 @@
+<?php
+    $x = 10;
+    $y = 2;
+    $z = null;
+    echo "{$z} <br>";
+    $z = $x + $y;
+    echo "{$z} <br>";
+    $z = $x - $y;
+    echo "{$z} <br>";
+    $z = $x * $y;
+    echo "{$z} <br>";
+    $z = $x ** $y;
+    echo "{$z} <br>";
+    $z = $x % $y;
+    echo "{$z} <br>";
+
+?>
