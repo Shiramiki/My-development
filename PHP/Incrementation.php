04c1d49045d76a5ae89389fab403ddf26060a8db
Isooba Mbeiza Rachel <123367383+Shiramiki@users.noreply.github.com>
2025-06-03 18:55:39 +0300

    lesson 4

---
+++ PHP/Incrementation.php
@@ -0,0 +1,21 @@
+<?php
+    $counter =1;
+    $counter = $counter + 1;
+        echo "{$counter} <br>";
+    $counter ++;
+    echo "{$counter} <br>";
+    $counter --;
+    echo "{$counter} <br>";
+    $counter+=2;
+    echo "{$counter} <br>";
+    $counter-=3;
+    echo "{$counter} <br>";
+    $total = 1+2-3*4/5**6;
+    echo $total;
+    
+    // operator precedence
+    // ()
+    // **
+    // */%
+    // +-
+?>
